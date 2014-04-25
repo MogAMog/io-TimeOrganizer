fa87c977927eecf3cea6b94cf2caae5b39e119d1
Slawex <slawomirbukowiec@gmail.com>
2014-04-25 10:14:12 +0200

    Layouts for HelpActivity

--- IOProject/src/pl/edu/agh/view/help/HelpItem.java
+++ IOProject/src/pl/edu/agh/view/help/HelpItem.java
@@ -6,8 +6,12 @@ import com.example.ioproject.R;
 
 public enum HelpItem implements Serializable {
 	
-	LOCATION_HELP(R.string.EventDate_Location, R.drawable.globe, R.layout.help_content_localization),
-	ADD_EVENT_HELP(R.string.Event_Title, R.drawable.add, R.layout.help_content_addevent);
+	LOCATION_HELP(R.string.HelpView_HelpItemTitle_ShowLocation, R.drawable.globe, R.layout.help_content_localization),
+	ADD_EVENT_HELP(R.string.HelpView_HelpItemTitle_AddEvent, R.drawable.add, R.layout.help_content_addevent),
+	DELETE_EVENT_HELP(R.string.HelpView_HelpItemTitle_DeleteEvent, R.drawable.delete_event, R.layout.help_content_delete_event),
+	ADD_DEFAULT_LOCATION(R.string.HelpView_HelpItemTitle_DefaultLocations, R.drawable.location_map, R.layout.help_content_default_locations),
+	SHOW_EVENT_INFO(R.string.HelpView_HelpItemTitle_ShowEventInfo, R.drawable.calendar_info, R.layout.help_content_show_event_info),
+	GO_BACK(R.string.HelpView_HelpItemTitle_GoBack, R.drawable.arrow_back, R.layout.help_content_goback);
 	
 	private final int titleID; //string.xml
 	private final int pictureID;	//res/drawable/*
